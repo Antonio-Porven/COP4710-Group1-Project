062abf62474ac4c32fc04fbde9256362935f5071
Antonio Porven <92689445+Antonio-Porven@users.noreply.github.com>
2023-10-10 21:26:37 -0400

    Add files via upload
    Added a login and sign up page, sign up page kinda wonky. No database connection yet.

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return '<h1>Hello World!</h1>'
+    return render_template('home.html')
 
+@app.route('/login')
+def login():  # put application's code here
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():  # put application's code here
+    return render_template('signup.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
