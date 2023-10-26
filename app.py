754c5a0072cba42ba0b363f2312d6dd60cf87b1b
Antonio Porven <92689445+Antonio-Porven@users.noreply.github.com>
2023-10-26 18:37:48 -0400

    Add files via upload
    Added the database all the connectors are installed to ensure compatibility. Password of the server connection should be changed to ensure the ability for you to be able to connect to it successfully. Database should be functional but further testing is needed.

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, redirect, url_for, request
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#importing the database
+#change password to your database password this is just a default password
+app.config ["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:password@localhost/universityDB'
 
-
+#dummy page really does nothing
 @app.route('/home')
 def hello_world():  # put application's code here
     return render_template('home.html')
 
+
+#Login Page
 @app.route('/login')
 @app.route('/')
 def login():
@@ -19,6 +24,8 @@ def login():
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
+
+#Signup Page
 @app.route('/signup')
 def signup():  # put application's code here
     return render_template('signup.html')
